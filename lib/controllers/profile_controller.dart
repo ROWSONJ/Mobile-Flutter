6e88089711be6ad8266fce1fe73d73c9e4a4af7a
Ajcharee Pakapikul <erne.ajcharee02@gmail.com>
2024-04-01 19:41:57 +0700

    profile

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,23 +1,22 @@
-import 'dart:ffi';
-
-import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-
-import '../services/user_repository.dart';
-
-class ProfileController extends GetxController {
-  static ProfileController get instonce => Get.find();
-
-  final _authRepo = Get.put(AuthenticationRepository()); //error อยู่แล้วข้ามไปได้เลย
-  final _userRepo = Get.put(UserRepository());
-
-  /// Step 3 - Get User Emoil ond pass to UserRepository to fetch user record.
-  getUserData() {
-    final email = _authRepo.firebaseUser.value ?. email;
-    if(email != null){
-      return _userRepo.getUserDetails(email);
-    } else {
-      Get.snackbar("Error", "Login to continue");
-    }
-  }
-}
+// import 'dart:ffi';
+//
+// import 'package:get/get.dart';
+// import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+//
+// import '../services/user_repository.dart';
+//
+// class ProfileController extends GetxController {
+//   static ProfileController get instonce => Get.find();
+//
+//   final _userRepo = Get.put(UserRepository());
+//
+//   /// Step 3 - Get User Emoil ond pass to UserRepository to fetch user record.
+//   getUserData() {
+//     final email = _authRepo.firebaseUser.value ?. email;
+//     if(email != null){
+//       return _userRepo.getUserDetails(email);
+//     } else {
+//       Get.snackbar("Error", "Login to continue");
+//     }
+//   }
+// }
